840dbeb82d4b4a89bc6ec153ed34ee595d74836c
Tyler Healy <tyler_2319@yahoo.com>
2013-01-28 22:24:43 -0500

    New TransactionalFileInputStream code

--- 440-HW1/src/TransactionalFileInputStream.java
+++ 440-HW1/src/TransactionalFileInputStream.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.io.InputStream;
@@ -17,7 +18,17 @@ public class TransactionalFileInputStream extends InputStream implements Seriali
 	public int read() throws IOException {
 		// TODO Auto-generated method stub
 		
-		return 0;
+		FileInputStream fis = new FileInputStream(src);
+		byte[] bit = new byte[1];
+	    int result = fis.read(bit, byteCounter, 1);
+	    byteCounter++;
+	    fis.close();
+	    
+	    if (result == -1) {
+		  return result;
+	    } else {
+	    	return bit[0];
+	    }
 	}
 
 }
