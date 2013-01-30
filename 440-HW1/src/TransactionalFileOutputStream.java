6e48be025eb6284c29db8935375478b489dd98c6
Tyler Healy <tyler_2319@yahoo.com>
2013-01-30 18:08:22 -0500

    Fixed TransactionalFileOutputStream / Added GrepProcess

--- 440-HW1/src/TransactionalFileOutputStream.java
+++ 440-HW1/src/TransactionalFileOutputStream.java
@@ -7,17 +7,19 @@ import java.io.Serializable;
 public class TransactionalFileOutputStream extends OutputStream implements Serializable {
 	private int byteCounter;
 	private String src;
+	private boolean append;
 	
-	public TransactionalFileOutputStream(String src) {
+	public TransactionalFileOutputStream(String src, boolean append) {
 		byteCounter = 0;
 		this.src = src;
+		this.append = append;
 	}
 	
 	@Override
 	public void write(int inputByte) throws IOException {
 		// TODO Auto-generated method stub
 		
-		FileOutputStream fos = new FileOutputStream(src);
+		FileOutputStream fos = new FileOutputStream(src, append);
 		byte[] bit = new byte[1];
 		bit[0] = (byte)inputByte;
 	    fos.write(bit, byteCounter, 1);
