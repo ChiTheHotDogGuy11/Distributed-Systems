e73e5d7fbe37777ce751be442c170bf7d765df0e
Justin Greet <Justin@Justin-PC.(none)>
2013-02-01 01:15:56 -0500

    Fix conflict in SlaveListener

--- 440-HW1/src/SlaveListener.java
+++ 440-HW1/src/SlaveListener.java
@@ -35,49 +35,50 @@ public class SlaveListener {
 			public void run() {
 				running = true;
 				while(running) {
-					PrintWriter out = null;
-					BufferedReader in = null;
-					ObjectInputStream oin = null;
-					String request = "";
-					
-					try {
-						out = new PrintWriter(sck.getOutputStream(), true);
-						in = new BufferedReader(new InputStreamReader(sck.getInputStream()));
-						request = in.readLine();
-						in.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					
-					if (request == "NumProcesses?\n") {
-						out.println(pm.getThreads().size());
-					} else if (request == "incoming") {
-						Thread newProcess = null;
+					if (!sck.isClosed()) {
+						PrintWriter out = null;
+						BufferedReader in = null;
+						ObjectInputStream oin = null;
+						String request = "";
+						
 						try {
-							oin = new ObjectInputStream(sck.getInputStream());
-							newProcess = (Thread) oin.readObject();
-							oin.close();
-							pm.addProcess(newProcess);
+							out = new PrintWriter(sck.getOutputStream(), true);
+							in = new BufferedReader(new InputStreamReader(sck.getInputStream()));
+							request = in.readLine();
+							in.close();
 						} catch (IOException e) {
 							e.printStackTrace();
-						} catch (ClassNotFoundException e) {
-							e.printStackTrace();
 						}
-					} else {
-						try { 
-							String[] reqArray = request.split(" ");
-							out.close();
-							if (reqArray.length == 2 && reqArray[0] == "migrate") {
-								int over = Integer.parseInt(reqArray[1]);
-								for (int i = 0; i < over; i++) {
-										pm.migrate();
-								}
+						
+						if (request == "NumProcesses?\n") {
+							out.println(pm.getThreads().size());
+						} else if (request == "incoming") {
+							Thread newProcess = null;
+							try {
+								oin = new ObjectInputStream(sck.getInputStream());
+								newProcess = (Thread) oin.readObject();
+								oin.close();
+								pm.addProcess(newProcess);
+							} catch (IOException e) {
+								e.printStackTrace();
+							} catch (ClassNotFoundException e) {
+								e.printStackTrace();
 							}
-						} catch (IOException e) {
-							e.printStackTrace();
+						} else {
+							try { 
+								String[] reqArray = request.split(" ");
+								if (reqArray.length == 2 && reqArray[0] == "migrate") {
+									int over = Integer.parseInt(reqArray[1]);
+									for (int i = 0; i < over; i++) {
+											pm.migrate();
+									}
+								}
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
 						}
+						out.close();
 					}
-					out.close();
 				}
 			}
 		});
