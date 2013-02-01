600e19bd6a00e407b6d01bade0924aea85a55206
Tyler Healy <tyler_2319@yahoo.com>
2013-01-31 21:44:04 -0500

    ServerSocketWrapper documentation

--- 440-HW1/src/ServerSocketWrapper.java
+++ 440-HW1/src/ServerSocketWrapper.java
@@ -4,21 +4,50 @@ import java.net.Socket;
 import java.sql.Connection;
 import java.util.ArrayList;
 
+/** ServerSocketWrapper
+ * 
+ * Wrapper class for ServerSocket
+ * Allows it to be started and stopped in parallel to the ProcessManager
+ * accepting commands.
+ * 
+ * @author Tyler Healy (thealy)
+ */
 public class ServerSocketWrapper {
 
+	//Thread in which the process will be run
 	private Thread thread;
+	
+	//ServerSocket that will be accepting connections
     private ServerSocket server;
+    
+    //boolean that determines whether the main thread should run
     private volatile boolean running;
+    
+    //List of connections made to the master ProcessManager
     private ArrayList<Socket> sockets = new ArrayList<Socket>();
     
+    //port, backlog for the Socket connection
     private int port;
     private int backlog;
 	
+    /** ServerSocketWrapper(int port, int backlog)
+     * 
+     * Constructor that accepts a port and backlog for the Socket connection
+     * @param port - port for the Socket connection
+     * @param backlog - backlog for the Socket connection
+     */
 	public ServerSocketWrapper(int port, int backlog) {
 		this.port = port;
 		this.backlog = backlog;
 	}
 	
+	/** start()
+	 * 
+	 * Starts the process of accepting connections by running server.accept
+	 * within a Thread
+	 * @throws Exception - thrown when start is attempted and the process is
+	 * 					   already running
+	 */
 	public synchronized void start() throws Exception {
 		if (thread != null) {
 			throw new Exception("ServerSocket already started.");
@@ -27,6 +56,10 @@ public class ServerSocketWrapper {
 		server = new ServerSocket(port, backlog);
 		thread = new Thread(new Runnable() {
 			@Override
+			/** run()
+			 * 
+			 * Accepts connections and adds it to the managed list
+			 */
 			public void run() {
 				running = true;
 				while(running) {
@@ -40,6 +73,10 @@ public class ServerSocketWrapper {
 		thread.start();
 	}
 	
+	/** stop()
+	 * 
+	 * Stops the ServerSocket from accepting connections and closes it
+	 */
 	public synchronized void stop() {
 		if (thread == null) {
 			return;
@@ -54,6 +91,10 @@ public class ServerSocketWrapper {
 		thread = null;
 	}
 	
+	/** getSockets()
+	 * 
+	 * @return a list of the connections to the master ProcessManager
+	 */
 	public synchronized ArrayList<Socket> getScokets() {
 		return sockets;
 	}
