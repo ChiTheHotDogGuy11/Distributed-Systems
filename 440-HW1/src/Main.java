e6720f0b222c53e443bc12840718afabcc3ff16f
Tyler Healy <tyler_2319@yahoo.com>
2013-02-01 00:36:15 -0500

    Attempt to fix error

--- 440-HW1/src/Main.java
+++ 440-HW1/src/Main.java
@@ -6,13 +6,13 @@ public class Main {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-		/*ProcessManager pm = new ProcessManager();
-		pm.receiveCommands();*/
-		String[] args2 = new String[2];
+		ProcessManager pm = new ProcessManager();
+		pm.receiveCommands();
+		/*String[] args2 = new String[2];
 		args2[0] = "http://www.justingreet.com";
 		args2[1] = "silly.txt";
 		WebCrawler heyThere = new WebCrawler(args2);
-		heyThere.run();
+		heyThere.run();*/
 		
 	}
 
